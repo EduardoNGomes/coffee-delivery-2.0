095cb821f0e6ade2fb02941d8aa776f5574f5bbf
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-04-03 11:55:30 -0300

    add product from stripe && change svg for phosphor icons create types

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // use stripe images
+  images: {
+    domains: ['files.stripe.com']
+  }
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'base-title': '#272221',
+        'base-sub-title': '#403937',
+        'base-text': '#574f4d',
+        'base-label': '#8d8686',
+        'base-hover': '#d7d5d5',
+        'base-button': '#e6e5e5',
+        'base-input': '#ededed',
+        'base-card': '#f3f2f2',
+        background: '#fafafa'
+      }
+    }
   },
   plugins: []
 }
