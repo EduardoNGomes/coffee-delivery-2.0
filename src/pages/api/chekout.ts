cf08a711fe9d62232e8136dd90e268123c9ca9a4
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-04-11 14:51:02 -0300

    Create Next API

---
+++ src/pages/api/chekout.ts
@@ -0,0 +1,30 @@
+import { stripe } from '@/lib/stripe'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { productsList } = req.body
+
+  if (req.method !== `post`) {
+    return res.status(405).json({ error: `method not allowed` })
+  }
+
+  if (!productsList) {
+    return res.status(400).json({ error: `productsList not found` })
+  }
+
+  const successUrl = 'http://localhost:3000/success'
+  const cancelUrl = 'http://localhost:3000/success'
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: productsList,
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+  })
+
+  return res.status(201).json({
+    checkoutUrl: session.url,
+  })
+}
